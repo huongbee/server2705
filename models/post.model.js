0d11a4be2da30a7ea0cf1b68797742237de15189
Huong <huongnguyenak96@gmail.com>
2019-07-17 21:19:44 +0700

    create post

--- models/post.model.js
+++ models/post.model.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { UserModel } = require('./user.model')
+
 const PostSchema = new Schema({
     author: {
         type: Schema.Types.ObjectId,
@@ -15,4 +17,32 @@ const PostSchema = new Schema({
     ]
 })
 const PostModel = mongoose.model('post', PostSchema);
-module.exports = { PostModel }
+class Post{
+    /**
+     * 
+     * @param {*} author user id
+     * @param {*} content content's post
+     */
+    static async createPost(author,content) {
+        if(!author || author == '')
+            throw new Error('Missing author!');
+        if(!content || content == '')
+            throw new Error('Plz fill content!');
+        if(!mongoose.Types.ObjectId.isValid(author))
+            throw new Error('User id invalid!');
+        const user = await UserModel.findById(author);
+        if(!user) throw new Error('Can not find author!');
+        const post = await PostModel.create({author, content})
+        if(!post) throw new Error('Can not create post!');
+        const author = UserModel.findByIdAndUpdate(
+            author,
+            { $addToSet: { posts: post._id }}
+        )
+        if(!post) throw new Error('Can not update author!');
+        return {
+            _id: post._id,
+            content: post.content
+        }
+    }
+}
+module.exports = { PostModel, Post }
--- models/user.model.js
+++ models/user.model.js
@@ -13,7 +13,6 @@ const UserSchema = new Schema({
     name: String,
     posts: [
         { type: Schema.Types.ObjectId, ref: 'post' }
-        // post: collection name
     ],
     friends: [
         { type: Schema.Types.ObjectId, ref: 'user' }
